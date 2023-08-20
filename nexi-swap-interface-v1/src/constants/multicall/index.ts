d6664cc173191ce06cde882505becf7a729024ee
git <eae966@gmail.com>
2023-08-20 20:39:57 +0330

    Multicall has been set

--- nexi-swap-interface-v1/src/constants/multicall/index.ts
+++ nexi-swap-interface-v1/src/constants/multicall/index.ts
@@ -2,8 +2,8 @@ import { ChainId } from '@nexiswap/sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x90F420222b60716D36f591eE0822663c49A6d7Ad', // TODO
-  [ChainId.TESTNET]: '0x90F420222b60716D36f591eE0822663c49A6d7Ad'
+  [ChainId.MAINNET]: '0x361a27AEBf510Ae5e7768B0a4d24E402bE07b56E', // TODO
+  [ChainId.TESTNET]: '0x361a27AEBf510Ae5e7768B0a4d24E402bE07b56E'
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
